2ba5442d1722979750c3bf36c1f2e83e649667b9
Wendy <wendy.calderon@esinergia.co>
2021-02-28 22:20:01 -0500

    Modificacion de estandar

--- web/modules/custom/co_core/src/Unit.php
+++ web/modules/custom/co_core/src/Unit.php
@@ -5,27 +5,32 @@ namespace Drupal\co_core;
 /**
  * Defines a Unit class.
  */
-class Unit {
+class Unit
+{
+    private $length = 0;
 
-  private $length = 0;
+    /**
+     * @param int $length
+     */
+    public function setLength(int $length)
+    {
+        $this->length = $length;
+    }
 
-  /**
-   * @param int $length
-   */
-  public function setLength(int $length) {
-    $this->length = $length;
-  }
-
-  /**
-   * @return int
-   *   The length of the unit.
-   */
-  public function getLength() {
-    return $this->length;
-  }
-
+    /**
+     * @return int
+     *   The length of the unit.
+     */
+    public function getLength()
+    {
+        return $this->length;
+    }
 }
 
 //https://salferrarello.com/setup-must-be-compatible-with-phpunitframeworktestcasesetup/
 //https://www.lullabot.com/articles/an-introduction-to-unit-testing-in-drupal
 //https://www.axelerant.com/resources/team-blog/drupal-8-writing-your-first-unit-test-with-phpunit
+
+
+//Calidad codigo
+//drupalcbf modules/custom/co_core/tests/src/Unit/UnitTest.php
